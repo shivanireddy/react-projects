9116bc52eb1a90c3310c934b14639026eca3ac46
Shivani Reddy <shivanireddy010197@gmail.com>
2021-11-21 10:18:30 -0500

    basic react

---
+++ advice-generator/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+    state = { advice: '' };
+
+    componentDidMount() {
+        console.log('Component did mount');
+    }
+    render() {
+        return (
+            <h1>App</h1>
+        );
+    }
+}
+
+export default App;
