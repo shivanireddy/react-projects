b9f57c039fe98fcc6c4e67883defd9fa0add6c5c
Shivani <shivanireddy010197@gmail.com>
2023-02-19 01:37:13 -0500

    added list of items and rating

--- travel-advisor/src/components/Map/Map.jsx
+++ travel-advisor/src/components/Map/Map.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import GoogleMapReact  from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)')
+  const coordinates = { lat: 0, lng: 0}
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact 
+        bootstrapURLKeys={{key:'AIzaSyCsH_rtIC8nlmPxMkEN1FIUii91xfXq7nM'}}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+        >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
