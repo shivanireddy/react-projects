e3498b65ae89ae04ab3e1b6157436de8e382be0a
Shivani <shivanireddy010197@gmail.com>
2023-04-01 10:17:36 -0400

    basic page with listings

---
+++ realestate/utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Key': '3366cb26bcmsh0bc815c998aac95p186c2bjsn38fa110e83e8',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+          }
+        });
+    return data;
+}
